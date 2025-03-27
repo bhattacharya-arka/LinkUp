5b41595bfb39c09f573abe8f6e472ea482fe8b5a
Arka Bhattacharya <arkabhattacharya19@gmail.com>
2025-03-27 13:18:17 +0530

    added the db and some basic backend stuff

---
+++ LinkAPI/Controllers/ActivitiesController.cs
@@ -0,0 +1,22 @@
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace LinkAPI.Controllers;
+
+public class ActivitiesController(AppDbContext context) : BaseAPIController
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await context.Activities.ToListAsync();
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivity(string id)
+    {
+        var activity = await context.Activities.FindAsync(id);
+        if(activity == null) return NotFound();
+        return activity;
+    }
+}
---
+++ LinkAPI/Controllers/BaseAPIController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace LinkAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseAPIController : ControllerBase
+    {
+    }
+}
